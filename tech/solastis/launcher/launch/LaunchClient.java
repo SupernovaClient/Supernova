e7808f87da1b193aededb744dc107e2cafca8f08
FemboyCoder <royaltywaaves@outlook.com>
2022-03-23 07:28:15 +0000

    Launcher API added mac support

--- tech/solastis/launcher/launch/LaunchClient.java
+++ tech/solastis/launcher/launch/LaunchClient.java
@@ -5,12 +5,18 @@ import net.minecraft.client.main.Main;
 import java.io.File;
 
 public class LaunchClient {
-	
-	
+
 	private static final String APPDATA = System.getenv("APPDATA");
-	private static final File WORKING_DIRECTORY = new File(APPDATA, ".minecraft/");
+	private static final File WORKING_DIRECTORY_WIN = new File(APPDATA, ".minecraft/");
+
+	private static final boolean isMac = System.getProperty("os.name").contains("Mac");
+	private static final String USER_HOME = System.getProperty("user.home");
+	private static final File WORKING_DIRECTORY_MAC = new File(USER_HOME, "Library/Application Support/minecraft/");
 	
 	public static void launch() {
+
+		final File WORKING_DIRECTORY = isMac ? WORKING_DIRECTORY_MAC : WORKING_DIRECTORY_WIN;
+
 		Main.main(new String[]{
 				"--version", "1.8.9",
 				"--accessToken", "0", "--assetIndex", "1.8",
